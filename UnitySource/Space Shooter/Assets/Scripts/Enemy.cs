953d54dd4b83eefcf312f060465b6a6f1163d84a
iLigonis <karmonasedvinas@gmail.com>
2019-02-03 19:52:40 +0000

    Add Enemy class and edit Bullet class to deal dmg
    Base enemy class which only stores health and xp as well as
    destroies itself when health is < 0
    Bullet calls enemy's method to deal dmg to it

---
+++ UnitySource/Space Shooter/Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int healthPoints = 100;
+    public int xpWorth = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // TODO: Enemy movement and attacks
+    }
+
+    public void TakeDamage(int dmg)
+    {
+        healthPoints -= dmg;
+        if (healthPoints < 0)
+        {
+            Death();
+        }
+    }
+
+    private void Death()
+    {
+        // TODO: Give xp
+
+        Destroy(gameObject);
+    }
+}
